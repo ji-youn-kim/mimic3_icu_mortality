528ec16acd64203090b8b624f5c9e21048de32f2
Jiyoun Kim <0715jiyounkim@gmail.com>
2022-01-04 12:30:50 +0900

    RNN RETAIN code 1. Bundle general icu stay data in dataloader batch 2. [BUG] Reverse whole icu stay data; not only item id, value_num 3. auroc, auprc with predict_proba

--- src/test_rnn.py
+++ src/test_rnn.py
@@ -9,7 +9,6 @@ import time
 from tqdm import tqdm
 from sklearn.metrics import roc_auc_score, average_precision_score
 
-
 import rnn_options
 from utils.rnn_word2id import *
 
@@ -57,6 +56,7 @@ class ChartEventSequenceWithLabelDataset(Dataset):
         self.num_features = len(inputs)
 
         # if reverse = True, reverse item_ids, value_nums for RETAIN
+        # does not reverse padded elements(0) - elements only before padding are reversed
         if reverse:
             item_id_total = inputs[self.num_features - 3]
             value_num_total = inputs[self.num_features - 2]
@@ -85,42 +85,39 @@ def visit_collate_fn(batch):
     batch_los, batch_admit, batch_insurance, batch_lang, batch_religion, batch_marital, batch_ethnicity, \
         batch_diagnosis, batch_gender, batch_item_id, batch_value_num, batch_before_pad_len, batch_labels = zip(*batch)
 
-    zipped = zip(batch_item_id, batch_value_num, batch_before_pad_len)
-    zipped = sorted(zipped, key=lambda x: x[2], reverse=True)
-    batch_item_id, batch_value_num, batch_before_pad_len = zip(*zipped)
+    # sort icu stay data by chart events count descending order
+    zipped = zip(batch_before_pad_len, batch_los, batch_admit, batch_insurance, batch_lang, batch_religion, batch_marital,
+                 batch_ethnicity, batch_diagnosis, batch_gender, batch_item_id, batch_value_num, batch_labels)
+    zipped = sorted(zipped, key=lambda x: x[0], reverse=True)
+    batch_before_pad_len, batch_los, batch_admit, batch_insurance, batch_lang, batch_religion, batch_marital,\
+        batch_ethnicity, batch_diagnosis, batch_gender, batch_item_id, batch_value_num, batch_labels = zip(*zipped)
 
     # print("np array itemid, valuenum print")
     # print(np.array([np.array([b_id]) for b_id in batch_item_id]))
     # print(np.array([np.array([b_vn]) for b_vn in batch_value_num]))
 
-    batch_los = np.stack(np.array(batch_los), axis=0)
-    batch_admit = np.stack(np.array(batch_admit), axis=0)
-    batch_insurance = np.stack(np.array(batch_insurance), axis=0)
-    batch_lang = np.stack(np.array(batch_lang), axis=0)
-    batch_religion = np.stack(np.array(batch_religion), axis=0)
-    batch_marital = np.stack(np.array(batch_marital), axis=0)
-    batch_ethnicity = np.stack(np.array(batch_ethnicity), axis=0)
-    batch_diagnosis = np.stack(np.array(batch_diagnosis), axis=0)
-    batch_gender = np.stack(np.array(batch_gender), axis=0)
-    batch_item_id = np.stack(np.array([np.array([b_id]) for b_id in batch_item_id]).astype('int'), axis=0)
-    batch_value_num = np.stack(np.array([np.array([b_vn]) for b_vn in batch_value_num]).astype('float'), axis=0)
+    batch_general = [batch_los, batch_admit, batch_insurance, batch_lang, batch_religion, batch_marital,
+                     batch_ethnicity, batch_diagnosis, batch_gender]
+
+    # convert each list to np array
+    batch_general = np.array(batch_general)
+    batch_item_id = np.array([np.array([b_id]) for b_id in batch_item_id]).astype('int')
+    batch_value_num = np.array([np.array([b_vn]) for b_vn in batch_value_num]).astype('float')
 
     batch_item_id = np.squeeze(batch_item_id, axis=1)
     batch_value_num = np.squeeze(batch_value_num, axis=1)
-    # print("after stack")
-    # print(batch_item_id)
-    # print(type(batch_item_id))
-    # print(type(batch_item_id[0]))
-    # print(batch_item_id.shape)
+    print("after stack")
+    print(batch_item_id)
+    print(type(batch_item_id))
+    print(type(batch_item_id[0]))
+    print(batch_item_id.shape)
 
-    return torch.tensor(batch_los), torch.tensor(batch_admit), torch.tensor(batch_insurance), torch.tensor(batch_lang),\
-        torch.tensor(batch_religion), torch.tensor(batch_marital), torch.tensor(batch_ethnicity),\
-        torch.tensor(batch_diagnosis), torch.tensor(batch_gender), torch.tensor(batch_item_id), \
-        torch.tensor(batch_value_num), torch.tensor(batch_labels), batch_before_pad_len
+    return torch.tensor(batch_general), torch.tensor(batch_item_id), torch.tensor(batch_value_num), \
+        torch.tensor(batch_labels), batch_before_pad_len
 
 
 class RETAIN(nn.Module):
-    def __init__(self, dim_input, dict_len_list, dim_emb=256, dropout_input=0.8, dropout_emb=0.9, dim_alpha=256,
+    def __init__(self, dict_len_list, dim_input=256, dim_emb=256, dropout_input=0.8, dropout_emb=0.9, dim_alpha=256,
                  dim_beta=256, dropout_context=0.6, dim_output=2, l2=0.0001, batch_first=True):
         super(RETAIN, self).__init__()
         self.batch_first = batch_first
@@ -163,22 +160,23 @@ class RETAIN(nn.Module):
         sum_masked_exp = torch.sum(masked_exp, dim=1, keepdim=True)
         return masked_exp / sum_masked_exp
 
-    def forward(self, los, admit, insur, lang, rel, mar, eth, diag, gender, item, val, lengths):
+    def forward(self, general, item, val, lengths):
         batch_size = item.size(dim=0)
 
-        admit_emb = self.admit_embedding(admit)
-        insurance_emb = self.insurance_embedding(insur)
-        lang_emb = self.lang_embedding(lang)
-        rel_emb = self.religion_embedding(rel)
-        marital_emb = self.marital_embedding(mar)
-        eth_emb = self.ethnicity_embedding(eth)
-        diag_emb = self.diagnosis_embedding(diag)
-        gender_emb = self.gender_embedding(gender)
-        item_emb = self.item_id_embedding(item)
+        admit_emb = self.admit_embedding(general[1].int())
+        insurance_emb = self.insurance_embedding(general[2].int())
+        lang_emb = self.lang_embedding(general[3].int())
+        rel_emb = self.religion_embedding(general[4].int())
+        marital_emb = self.marital_embedding(general[5].int())
+        eth_emb = self.ethnicity_embedding(general[6].int())
+        diag_emb = self.diagnosis_embedding(general[7].int())
+        gender_emb = self.gender_embedding(general[8].int())
+        item_emb = self.item_id_embedding(item.int())
 
-        los = torch.unsqueeze(los, 1)
+        los = torch.unsqueeze(general[0], 1)
         # concatenate general features
-        x_general = torch.cat((admit_emb, insurance_emb, lang_emb, rel_emb, marital_emb, eth_emb, diag_emb, gender_emb, los), 1)
+        x_general = torch.cat((admit_emb, insurance_emb, lang_emb, rel_emb, marital_emb,
+                               eth_emb, diag_emb, gender_emb, los), 1)
         print("x_general.shape: ", x_general.shape)
         val = torch.unsqueeze(val, 2)
         # concatenate chart events
@@ -186,15 +184,19 @@ class RETAIN(nn.Module):
         print("val.shape: ", val.shape)
         x_chart_events = torch.cat((item_emb, val), 2)
         x_general = torch.unsqueeze(x_general, 1).expand(-1, 100, -1)
+        # concatenate all features per icu stay
+        # x shape: (batch_size, 100, concat_dim_input)
         x = torch.cat((x_general, x_chart_events), dim=2)
         print("x shape: ", x.shape)
         # print("x[0][10]: ", x[0][10])
+        # emb shape: (batch_size, 100, dim_emb)
         emb = self.embedding(x).float()
 
+        # length shape: batch_size, type: list
         print(lengths)
         packed_input = pack_padded_sequence(emb, lengths, batch_first=self.batch_first)
-        # g shape: (batch_size, 100, dim_emb)
         g, _ = self.rnn_alpha(packed_input)
+        # alpha_unpacked shape: (batch_size, 100, dim_alpha)
         alpha_unpacked, _ = pad_packed_sequence(g, batch_first=self.batch_first)
         # e shape: (batch_size, 100, 1)
         e = self.alpha_fc(alpha_unpacked)
@@ -211,9 +213,12 @@ class RETAIN(nn.Module):
         # h shape: (batch_size, 100, dim_beta)
         h, _ = self.rnn_beta(packed_input)
         beta_unpacked, _ = pad_packed_sequence(h, batch_first=self.batch_first)
-        # shape: (batch_size, 100, dim_emb)
+        # beta shape: (batch_size, 100, dim_emb)
         beta = torch.tanh(self.beta_fc(beta_unpacked))
 
+        # alpha size: (batch_size, 100, 1)
+        # beta shape: (batch_size, 100, dim_emb)
+        # emb shape: (batch_size, 100, dim_emb)
         # context shape: (batch_size, dim_emb)
         context = torch.bmm(torch.transpose(alpha, 1, 2), beta * emb).squeeze(1)
 
@@ -234,9 +239,9 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # define model and set to device
-    model = RETAIN(dim_input=256, dict_len_list=[admit_dict_len, insurance_dict_len, lang_dict_len, religion_dict_len,
-                                                 marital_dict_len, ethnicity_dict_len, diagnosis_dict_len,
-                                                 gender_dict_len, item_ids_dict_len])
+    model = RETAIN(dict_len_list=[admit_dict_len, insurance_dict_len, lang_dict_len, religion_dict_len,
+                                  marital_dict_len, ethnicity_dict_len, diagnosis_dict_len,
+                                  gender_dict_len, item_ids_dict_len])
     model = model.to(device)
 
     # define loss function, optimizer
@@ -251,42 +256,32 @@ def main():
 
     for e_i in range(op.epochs):
         for b_i, batch in enumerate(tqdm(train_loader)):
-            batch_los, batch_admit, batch_insurance, batch_lang, batch_religion, batch_marital, batch_ethnicity, \
-                batch_diagnosis, batch_gender, batch_item_id, batch_value_num, batch_labels, \
-                batch_before_pad_len = batch
+            batch_general, batch_item_id, batch_value_num, batch_labels, batch_before_pad_len = batch
 
-            batch_los = batch_los.to(device)
-            batch_admit = batch_admit.to(device)
-            batch_insurance = batch_insurance.to(device)
-            batch_lang = batch_lang.to(device)
-            batch_religion = batch_religion.to(device)
-            batch_marital = batch_marital.to(device)
-            batch_ethnicity = batch_ethnicity.to(device)
-            batch_diagnosis = batch_diagnosis.to(device)
-            batch_gender = batch_gender.to(device)
+            batch_general = batch_general.to(device)
             batch_item_id = batch_item_id.to(device)
             batch_value_num = batch_value_num.to(device)
             batch_labels = batch_labels.to(device)
 
             # output shape: (batch_size, dim_output)
-            output, alpha, beta = model(batch_los, batch_admit, batch_insurance, batch_lang, batch_religion,
-                                        batch_marital, batch_ethnicity, batch_diagnosis, batch_gender, batch_item_id,
-                                        batch_value_num, batch_before_pad_len)
+            output, alpha, beta = model(batch_general, batch_item_id, batch_value_num, batch_before_pad_len)
 
             print("dtype: ", output.dtype, batch_labels.dtype)
             # batch labels shape: (batch_size, dim_output)
             print("batch_labels: ", batch_labels.tolist())
             print("batch_labels.size(): ", batch_labels.size())
             loss = criterion(output, batch_labels.long())
-            labels.extend(batch_labels.tolist())
-            _, pred_labels = torch.max(F.softmax(output).data, 1)
-            outputs.extend(pred_labels.float().tolist())
-            print("pred_labels.size: ", pred_labels.size())
-            print("pred_labels: ", pred_labels.float().tolist())
+            # labels.extend(batch_labels.tolist())
+            # _, pred_labels = torch.max(F.softmax(output).data, 1)
+            # outputs.extend(pred_labels.float().tolist())
+            labels.append(batch_labels)
+            outputs.append(F.softmax(output).data)
+            # print("pred_labels.size: ", pred_labels.size())
+            # print("pred_labels: ", pred_labels.float().tolist())
             total_loss += loss.item()
             train_count += batch_labels.size(dim=0)
-            correct += torch.eq(batch_labels, pred_labels).sum().item()
-            print("correct elements: ", torch.eq(batch_labels, pred_labels).sum().item())
+            # correct += torch.eq(batch_labels, pred_labels).sum().item()
+            # print("correct elements: ", torch.eq(batch_labels, pred_labels).sum().item())
 
             optimizer.zero_grad()
             loss.backward()
@@ -294,10 +289,12 @@ def main():
             # print("Epoch: ", e_i, "Average Loss: ", total_loss / train_count)
 
         print("Total Train count: ", train_count)
-        print("Epoch: ", e_i, "Average Loss: ", total_loss/train_count, "Accuracy: ", correct/train_count)
+        print("Epoch: ", e_i+1, "Average Loss: ", total_loss/train_count)
 
-    train_auroc = roc_auc_score(labels, outputs)
-    train_auprc = average_precision_score(labels, outputs)
+    train_auroc = roc_auc_score(torch.cat(labels, 0).numpy(), torch.cat(outputs, 0).numpy()[:, 1])
+    train_auprc = average_precision_score(torch.cat(labels, 0).numpy(), torch.cat(outputs, 0).numpy()[:, 1])
+    # train_auroc = roc_auc_score(labels, outputs)
+    # train_auprc = average_precision_score(labels, outputs)
     print("RETAIN TRAIN AUROC: ", train_auroc)
     print("RETAIN TRAIN AUPRC: ", train_auprc)
 
@@ -308,41 +305,31 @@ def main():
     correct = 0
 
     for b_i, batch in enumerate(tqdm(test_loader)):
-        batch_los, batch_admit, batch_insurance, batch_lang, batch_religion, batch_marital, batch_ethnicity, \
-            batch_diagnosis, batch_gender, batch_item_id, batch_value_num, batch_labels, \
-            batch_before_pad_len = batch
+        batch_general, batch_item_id, batch_value_num, batch_labels, batch_before_pad_len = batch
 
-        batch_los = batch_los.to(device)
-        batch_admit = batch_admit.to(device)
-        batch_insurance = batch_insurance.to(device)
-        batch_lang = batch_lang.to(device)
-        batch_religion = batch_religion.to(device)
-        batch_marital = batch_marital.to(device)
-        batch_ethnicity = batch_ethnicity.to(device)
-        batch_diagnosis = batch_diagnosis.to(device)
-        batch_gender = batch_gender.to(device)
+        batch_general = batch_general.to(device)
         batch_item_id = batch_item_id.to(device)
         batch_value_num = batch_value_num.to(device)
         batch_labels = batch_labels.to(device)
 
         # output shape: (batch_size, dim_output)
-        output, alpha, beta = model(batch_los, batch_admit, batch_insurance, batch_lang, batch_religion,
-                                    batch_marital, batch_ethnicity, batch_diagnosis, batch_gender, batch_item_id,
-                                    batch_value_num, batch_before_pad_len)
+        output, alpha, beta = model(batch_general, batch_item_id, batch_value_num, batch_before_pad_len)
 
         # batch labels shape: (batch_size, dim_output)
         print("batch_labels: ", batch_labels.tolist())
         print("batch_labels.size(): ", batch_labels.size())
         loss = criterion(output, batch_labels.long())
-        test_labels.extend(batch_labels.tolist())
-        _, pred_labels = torch.max(F.softmax(output).data, 1)
-        test_outputs.extend(pred_labels.float().tolist())
-        print("pred_labels.size: ", pred_labels.size())
-        print("pred_labels: ", pred_labels.float().tolist())
+        # test_labels.extend(batch_labels.tolist())
+        # _, pred_labels = torch.max(F.softmax(output).data, 1)
+        # test_outputs.extend(pred_labels.float().tolist())
+        test_labels.append(batch_labels)
+        test_outputs.append(F.softmax(output).data)
+        # print("pred_labels.size: ", pred_labels.size())
+        # print("pred_labels: ", pred_labels.float().tolist())
         total_loss += loss.item()
         test_count += batch_labels.size(dim=0)
-        correct += torch.eq(batch_labels, pred_labels).sum().item()
-        print("correct elements: ", torch.eq(batch_labels, pred_labels).sum().item())
+        # correct += torch.eq(batch_labels, pred_labels).sum().item()
+        # print("correct elements: ", torch.eq(batch_labels, pred_labels).sum().item())
 
         optimizer.zero_grad()
         loss.backward()
@@ -350,12 +337,14 @@ def main():
         # print("Epoch: ", e_i, "Average Loss: ", total_loss / train_count)
 
     print("Total Test count: ", test_count)
-    print("Epoch: ", e_i, "Average Loss: ", total_loss / test_count, "Accuracy: ", correct / test_count)
+    print("Epoch: ", e_i+1, "Average Loss: ", total_loss / test_count)
 
-    test_auroc = roc_auc_score(test_labels, test_outputs)
-    test_auprc = average_precision_score(test_labels, test_outputs)
-    print("RETAIN TRAIN AUROC: ", test_auroc)
-    print("RETAIN TRAIN AUPRC: ", test_auprc)
+    test_auroc = roc_auc_score(torch.cat(test_labels, 0).numpy(), torch.cat(test_outputs, 0).numpy()[:, 1])
+    test_auprc = average_precision_score(torch.cat(test_labels, 0).numpy(), torch.cat(test_outputs, 0).numpy()[:, 1])
+    # test_auroc = roc_auc_score(test_labels, test_outputs)
+    # test_auprc = average_precision_score(test_labels, test_outputs)
+    print("RETAIN TEST AUROC: ", test_auroc)
+    print("RETAIN TEST AUPRC: ", test_auprc)
 
 
 main()
