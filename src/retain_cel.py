b75c88f69693a8896fb4dd0511a2521b1a07e225
Jiyoun Kim <0715jiyounkim@gmail.com>
2022-01-04 23:59:05 +0900

    RETAIN only chart events code add

--- src/retain_cel.py
+++ src/retain_cel.py
@@ -3,8 +3,8 @@ import torch
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-from tqdm import tqdm
 from sklearn.metrics import roc_auc_score, average_precision_score
+from tqdm import tqdm
 
 import rnn_options
 from utils.rnn_word2id import *
@@ -57,12 +57,16 @@ class ChartEventSequenceWithLabelDataset(Dataset):
         return len(self.labels)
 
     def __getitem__(self, index):
-        return self.los[index], self.admit[index], self.insurance[index], self.lang[index], self.religion[index],\
-                self.marital[index], self.ethnicity[index], self.diagnosis[index], self.gender[index], \
-                self.item_id[index], self.value_num[index], self.before_pad_len[index], self.labels[index]
+        if op.model_type == "retain_general":
+            return self.los[index], self.admit[index], self.insurance[index], self.lang[index], self.religion[index],\
+                    self.marital[index], self.ethnicity[index], self.diagnosis[index], self.gender[index], \
+                    self.item_id[index], self.value_num[index], self.before_pad_len[index], self.labels[index]
+
+        elif op.model_type == "retain_only_chart_events":
+            return self.item_id[index], self.value_num[index], self.before_pad_len[index], self.labels[index]
 
 
-def visit_collate_fn(batch):
+def general_collate_fn(batch):
     batch_los, batch_admit, batch_insurance, batch_lang, batch_religion, batch_marital, batch_ethnicity, \
         batch_diagnosis, batch_gender, batch_item_id, batch_value_num, batch_before_pad_len, batch_labels = zip(*batch)
 
@@ -88,32 +92,62 @@ def visit_collate_fn(batch):
         torch.tensor(batch_labels), batch_before_pad_len
 
 
+def chart_events_collate_fn(batch):
+    batch_item_id, batch_value_num, batch_before_pad_len, batch_labels = zip(*batch)
+
+    # sort icu stay data by chart events count descending order
+    zipped = zip(batch_before_pad_len, batch_item_id, batch_value_num, batch_labels)
+    zipped = sorted(zipped, key=lambda x: x[0], reverse=True)
+    batch_before_pad_len, batch_item_id, batch_value_num, batch_labels = zip(*zipped)
+
+    # convert each list to np array
+    batch_item_id = np.array([np.array([b_id]) for b_id in batch_item_id]).astype('int')
+    batch_value_num = np.array([np.array([b_vn]) for b_vn in batch_value_num]).astype('float')
+
+    batch_item_id = np.squeeze(batch_item_id, axis=1)
+    batch_value_num = np.squeeze(batch_value_num, axis=1)
+
+    return torch.tensor(batch_item_id), torch.tensor(batch_value_num), torch.tensor(batch_labels), batch_before_pad_len
+
+
 class RETAIN(nn.Module):
-    def __init__(self, dict_len_list, dim_input=op.dim_input, dim_emb=op.dim_emb, dropout_input=op.dropout_input,
+    def __init__(self, dict_len_list, dim_input=op.dim_input, dim_emb=op.ce_dim_emb, dropout_input=op.dropout_input,
                  dropout_emb=op.dropout_emb, dim_alpha=op.dim_alpha, dim_beta=op.dim_beta,
                  dropout_context=op.dropout_context, dim_output=op.dim_output, batch_first=True):
         super(RETAIN, self).__init__()
         self.batch_first = batch_first
 
         # embedding for categorical features
-        self.admit_embedding = nn.Embedding(dict_len_list[0], dim_input)
-        self.insurance_embedding = nn.Embedding(dict_len_list[1], dim_input)
-        self.lang_embedding = nn.Embedding(dict_len_list[2], dim_input)
-        self.religion_embedding = nn.Embedding(dict_len_list[3], dim_input)
-        self.marital_embedding = nn.Embedding(dict_len_list[4], dim_input)
-        self.ethnicity_embedding = nn.Embedding(dict_len_list[5], dim_input)
-        self.diagnosis_embedding = nn.Embedding(dict_len_list[6], dim_input)
-        self.gender_embedding = nn.Embedding(dict_len_list[7], dim_input)
-        self.item_id_embedding = nn.Embedding(dict_len_list[8], dim_input)
+        if op.model_type == "retain_general":
+            self.admit_embedding = nn.Embedding(dict_len_list[0], dim_input)
+            self.insurance_embedding = nn.Embedding(dict_len_list[1], dim_input)
+            self.lang_embedding = nn.Embedding(dict_len_list[2], dim_input)
+            self.religion_embedding = nn.Embedding(dict_len_list[3], dim_input)
+            self.marital_embedding = nn.Embedding(dict_len_list[4], dim_input)
+            self.ethnicity_embedding = nn.Embedding(dict_len_list[5], dim_input)
+            self.diagnosis_embedding = nn.Embedding(dict_len_list[6], dim_input)
+            self.gender_embedding = nn.Embedding(dict_len_list[7], dim_input)
+            self.item_id_embedding = nn.Embedding(dict_len_list[8], dim_input)
 
-        concat_dim_input = dim_input * 9 + 1 * 2
+            concat_dim_input = dim_input * 9 + 1 * 2
 
-        # final embedding for concatenated feature embeddings
-        self.embedding = nn.Sequential(
-            nn.Dropout(p=dropout_input),
-            nn.Linear(concat_dim_input, dim_emb).double(),
-            nn.Dropout(p=dropout_emb)
-        )
+            # final embedding for concatenated feature embeddings
+            self.embedding = nn.Sequential(
+                nn.Dropout(p=dropout_input),
+                nn.Linear(concat_dim_input, dim_emb).double(),
+                nn.Dropout(p=dropout_emb)
+            )
+
+        if op.model_type == "retain_only_chart_events":
+            self.item_id_embedding = nn.Embedding(dict_len_list[0], dim_input)
+            self.embedding_dropout = nn.Dropout(p=dropout_input)
+
+            concat_dim_input = dim_input + 1
+
+            self.embedding = nn.Sequential(
+                nn.Linear(concat_dim_input, dim_emb).double(),
+                nn.Dropout(p=dropout_emb)
+            )
 
         self.rnn_alpha = nn.GRU(input_size=dim_emb, hidden_size=dim_alpha, num_layers=1, batch_first=self.batch_first)
         self.alpha_fc = nn.Linear(in_features=dim_alpha, out_features=1)
@@ -136,30 +170,43 @@ class RETAIN(nn.Module):
     def forward(self, general, item, val, lengths):
         batch_size = item.size(dim=0)
 
-        admit_emb = self.admit_embedding(general[1].int())
-        insurance_emb = self.insurance_embedding(general[2].int())
-        lang_emb = self.lang_embedding(general[3].int())
-        rel_emb = self.religion_embedding(general[4].int())
-        marital_emb = self.marital_embedding(general[5].int())
-        eth_emb = self.ethnicity_embedding(general[6].int())
-        diag_emb = self.diagnosis_embedding(general[7].int())
-        gender_emb = self.gender_embedding(general[8].int())
-        item_emb = self.item_id_embedding(item.int())
+        if op.model_type == "retain_general":
+            admit_emb = self.admit_embedding(general[1].int())
+            insurance_emb = self.insurance_embedding(general[2].int())
+            lang_emb = self.lang_embedding(general[3].int())
+            rel_emb = self.religion_embedding(general[4].int())
+            marital_emb = self.marital_embedding(general[5].int())
+            eth_emb = self.ethnicity_embedding(general[6].int())
+            diag_emb = self.diagnosis_embedding(general[7].int())
+            gender_emb = self.gender_embedding(general[8].int())
+            item_emb = self.item_id_embedding(item.int())
 
-        los = torch.unsqueeze(general[0], 1)
-        # concatenate general features
-        x_general = torch.cat((admit_emb, insurance_emb, lang_emb, rel_emb, marital_emb,
-                               eth_emb, diag_emb, gender_emb, los), 1)
-        val = torch.unsqueeze(val, 2)
-        # concatenate chart events
-        x_chart_events = torch.cat((item_emb, val), 2)
-        x_general = torch.unsqueeze(x_general, 1).expand(-1, 100, -1)
-        # concatenate all features per icu stay
-        # x shape: (batch_size, 100, concat_dim_input)
-        x = torch.cat((x_general, x_chart_events), dim=2)
+            los = torch.unsqueeze(general[0], 1)
+            # concatenate general features
+            x_general = torch.cat((admit_emb, insurance_emb, lang_emb, rel_emb, marital_emb,
+                                   eth_emb, diag_emb, gender_emb, los), 1)
+            val = torch.unsqueeze(val, 2)
+            # concatenate chart events
+            x_chart_events = torch.cat((item_emb, val), 2)
+            x_general = torch.unsqueeze(x_general, 1).expand(-1, 100, -1)
+            # concatenate all features per icu stay
+            # x shape: (batch_size, 100, concat_dim_input)
+            x = torch.cat((x_general, x_chart_events), dim=2)
 
-        # emb shape: (batch_size, 100, dim_emb)
-        emb = self.embedding(x).float()
+            # emb shape: (batch_size, 100, dim_emb)
+            emb = self.embedding(x).float()
+
+        if op.model_type == "retain_only_chart_events":
+            item_emb = self.item_id_embedding(item.int())
+            item_emb = self.embedding_dropout(item_emb)
+
+            val = torch.unsqueeze(val, 2)
+            # concatenate chart events
+            # x shape: (batch_size, 100, dim_input + 1)
+            x = torch.cat((item_emb, val), 2)
+
+            # emb shape: (batch_size, 100, dim_emb)
+            emb = self.embedding(x).float()
 
         # length shape: batch_size, type: list
         packed_input = pack_padded_sequence(emb, lengths, batch_first=self.batch_first)
@@ -200,16 +247,28 @@ def main():
     # define dataset, dataloader
     train_set = ChartEventSequenceWithLabelDataset(x_train_rnn, y_train)
     test_set = ChartEventSequenceWithLabelDataset(x_test_rnn, y_test)
-    train_loader = DataLoader(dataset=train_set, batch_size=op.batch_size, shuffle=True, collate_fn=visit_collate_fn)
-    test_loader = DataLoader(dataset=test_set, batch_size=op.batch_size, shuffle=True, collate_fn=visit_collate_fn)
 
     # define device type - gpu, cpu
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # define model and set to device
-    model = RETAIN(dict_len_list=[admit_dict_len, insurance_dict_len, lang_dict_len, religion_dict_len,
-                                  marital_dict_len, ethnicity_dict_len, diagnosis_dict_len,
-                                  gender_dict_len, item_ids_dict_len])
+    if op.model_type == "retain_general":
+        train_loader = DataLoader(dataset=train_set, batch_size=op.batch_size, shuffle=True,
+                                  collate_fn=general_collate_fn)
+        test_loader = DataLoader(dataset=test_set, batch_size=op.batch_size, shuffle=True,
+                                 collate_fn=general_collate_fn)
+        # define model and set to device
+        model = RETAIN(dict_len_list=[admit_dict_len, insurance_dict_len, lang_dict_len, religion_dict_len,
+                                      marital_dict_len, ethnicity_dict_len, diagnosis_dict_len,
+                                      gender_dict_len, item_ids_dict_len])
+
+    elif op.model_type == "retain_only_chart_events":
+        train_loader = DataLoader(dataset=train_set, batch_size=op.batch_size, shuffle=True,
+                                  collate_fn=chart_events_collate_fn)
+        test_loader = DataLoader(dataset=test_set, batch_size=op.batch_size, shuffle=True,
+                                 collate_fn=chart_events_collate_fn)
+        # define model and set to device
+        model = RETAIN(dict_len_list=[item_ids_dict_len])
+
     model = model.to(device)
 
     # define loss function, optimizer
@@ -224,7 +283,12 @@ def main():
         total_loss = 0
         train_count = 0
         for b_i, batch in enumerate(tqdm(train_loader)):
-            batch_general, batch_item_id, batch_value_num, batch_labels, batch_before_pad_len = batch
+            if op.model_type == "retain_general":
+                batch_general, batch_item_id, batch_value_num, batch_labels, batch_before_pad_len = batch
+
+            if op.model_type == "retain_only_chart_events":
+                batch_item_id, batch_value_num, batch_labels, batch_before_pad_len = batch
+                batch_general = torch.tensor([])
 
             batch_general = batch_general.to(device)
             batch_item_id = batch_item_id.to(device)
@@ -271,7 +335,12 @@ def main():
 
     with torch.no_grad():
         for b_i, batch in enumerate(tqdm(test_loader)):
-            batch_general, batch_item_id, batch_value_num, batch_labels, batch_before_pad_len = batch
+            if op.model_type == "retain_general":
+                batch_general, batch_item_id, batch_value_num, batch_labels, batch_before_pad_len = batch
+
+            if op.model_type == "retain_only_chart_events":
+                batch_item_id, batch_value_num, batch_labels, batch_before_pad_len = batch
+                batch_general = torch.tensor([])
 
             batch_general = batch_general.to(device)
             batch_item_id = batch_item_id.to(device)
